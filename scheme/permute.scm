9ba9e8c22895c2008b789092691f3f6db467d619
Bert Shim <bertshim@gmail.com>
2025-09-22 23:32:24 +0900

    Update permute.scm

--- scheme/permute.scm
+++ scheme/permute.scm
@@ -1,12 +1,10 @@
 (define (perm-aux head lst result)
     (cond ((and (null? head) (null? lst)) (cons result '() ))
           ((or (null? head) (null? lst)) '() )
-          (else 
-            (append 
+          (else (append 
                (perm-aux (remove (car head) lst )
                          (remove (car head) lst)
                          (cons (car head) result ) )
                (perm-aux (cdr head) lst result) ) ) ) )
-
 (define (perm lst)
         (perm-aux lst lst '() ) )
